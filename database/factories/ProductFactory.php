29670f57ad19b9d825f50426f792d071b5ead73e
nick@digitalhouse.com <nick@digitalhouse.com>
2018-05-18 18:35:57 -0300

    Fix Seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Category;
 use App\Product;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) use ($factory)
 {
+    $user_id = factory(User::class)->create()->id;
     return [
         'category_id' => function () { return factory(Category::class)->create()->id; },
-        'created_by' => $user_id = products_factory_user()->id,
+        'created_by' => $user_id,
         'updated_by' => $user_id,
         'tags' => $faker->word . ',' . $faker->word . ',' . $faker->word,
         'brand' => $faker->randomElement(['Apple', 'Microsoft', 'Samsung', 'Lg']),
-        'condition' => $faker->randomElement(['new', 'used', 'refurbished']),
         'low_stock' => $faker->randomElement([5, 10, 15]),
         'sale_counts'  => $faker->randomNumber(9),
         'view_counts'  => $faker->randomNumber(9),
