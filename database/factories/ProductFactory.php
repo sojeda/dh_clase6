b8851a6d7b7b875450989b67d50b78edabff6dcf
Sergio Ojeda <soj3da@gmail.com>
2018-05-18 17:27:55 -0300

    Seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,14 +19,6 @@ $factory->define(Product::class, function (Faker $faker) use ($factory)
         'description' => $faker->text(490),
         'name' => $faker->text(90),
         'cost' => rand(100, 500),
-        'price' => rand(500, 1000),
-        'features' => json_encode([
-            trans('globals.product_features.weight') => $faker->numberBetween(10, 150).' '.$faker->randomElement(['Mg', 'Gr', 'Kg', 'Oz', 'Lb']),
-            trans('globals.product_features.dimensions') => $faker->numberBetween(1, 30).' X '.
-                          $faker->numberBetween(1, 30).' X '.
-                          $faker->numberBetween(1, 30).' '.
-                          $faker->randomElement(['inch', 'mm', 'cm']),
-            trans('globals.product_features.color') => $faker->safeColorName,
-        ]),
+        'price' => rand(500, 1000),        
     ];
 });
